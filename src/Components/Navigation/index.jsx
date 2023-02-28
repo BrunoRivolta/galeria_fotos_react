80baac1432ebb5b49ab30996315418b8a0cccdc5
Bruno Rivolta <brrivolta@gmail.com>
2023-02-28 12:17:09 -0300

    Iniciando Projeto

---
+++ src/Components/Navigation/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navigation() {
+  return (
+    <nav>
+        <ul>
+            <li>Logo</li>
+            <li>Galeria</li>
+            <li>Favoritos</li>
+            <li>Sobre</li>
+        </ul>
+    </nav>
+  )
+}
---
+++ src/Pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './PaginaInicial.module.scss'
+
+export default function PaginaInicial() {
+  return (
+    <>
+        <div>PaginaInicial</div>
+        <h1 className='titulo'>ola</h1>
+    </>
+  )
+}
